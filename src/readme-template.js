d98f6a36a3c9a46b42bf7f4676bf27fc81301936
dcharney <delaneycharney@gmail.com>
2020-12-19 00:00:05 -0800

    license response of NONE will not generate badge or license section

--- src/readme-template.js
+++ src/readme-template.js
@@ -1,35 +1,25 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-    if (license) {
-        return `![GitHub license](https://img.shields.io/badge/License-${license}-blue.svg)`
+    if (license == "NONE") {
+        return ''
     } else {
-        return ""
+        return `![GitHub license](https://img.shields.io/badge/License-${license}-blue.svg)`
     };
-}
-
-// // TODO: Create a function that returns the license link
-// // If there is no license, return an empty string
-// function renderLicenseLink(license) {
-//     if (license) {
-//         return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`
-//     } else {
-//         return ""
-//     };
-// }
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-    if (license) {
+    if (license == "NONE") {
+        return ''
+    } else {
         return `
 ## License
 
 Licensed under the [${license}](./license.txt) license.`
-    } else {
-        return ""
     };
-}
+};
 
 // TODO: Create a function to generate markdown for README
 module.exports = data => {
