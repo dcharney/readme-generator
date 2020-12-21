02ae7929a454152066a3d36721e425eb51e9393c
dcharney <delaneycharney@gmail.com>
2020-12-21 00:16:52 -0800

    user prompts functioning as desired

--- index.js
+++ index.js
@@ -1,68 +1,10 @@
 // link required packages
 const inquirer = require('inquirer');
+const promptUser = require('./utils/user-prompts.js');
 const generateReadme = require('./src/readme-template.js');
 const writeFile = require('./utils/generate-readme.js');
-const log = require('single-line-log').stdout;
+// const log = require('single-line-log').stdout;
 
-// TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: 'input',
-        name: 'username',
-        message: 'What is your GitHub username?'
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is your email address?'
-    },
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is your project\'s title? (Required)',
-        validate: title => {
-            if (title) {
-                return true;
-            } else {
-                console.log('Please enter a project title!');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'What does your project do? Enter a short description:'
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'How should your project be installed?'
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Please enter usage information:'
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'What kind of license does your project have?',
-        choices: ['MIT','APACHE 2.0','GPL 3.0','BSD 3','NONE']
-    },
-    {
-        type: 'input',
-        name: 'contribution',
-        message: 'Please enter the contribution guidelines:'
-    },
-    {
-        type: 'input',
-        name: 'test',
-        message: 'Please enter the test instructions:'
-    }
-];
-
-const promptUser = () => inquirer.prompt(questions);
 console.log(`
 ============================================
     ~ WELCOME TO THE README GENERATOR ~
@@ -74,21 +16,23 @@ prompts to generate the best readme file.
 `)
 
 promptUser()
-    .then(answers => {
-        log('Building readme contents...');
-        return generateReadme(answers)
-    })
-    .then(readmeContents => {
-        log('Generating markdown file...');
-        return writeFile(readmeContents)
-    })
-    .then(response => {
-        log(response.message);
-        console.log(`
------------------
-Congratulations, your completed readme file is now located in the /dist folder. Happy Coding!`);
-    })
-    .catch(err => {
-        console.log(err);
+    .then(data => {
+        console.log(data);
     });
-
+//     .then(answers => {
+//         log('Building readme contents...');
+//         return generateReadme(answers)
+//     })
+//     .then(readmeContents => {
+//         log('Generating markdown file...');
+//         return writeFile(readmeContents)
+//     })
+//     .then(response => {
+//         log(response.message);
+//         console.log(`
+// -----------------
+// Congratulations, your completed readme file is now located in the /dist folder. Happy Coding!`);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     });
---
+++ utils/user-prompts.js
@@ -0,0 +1,169 @@
+const inquirer = require('inquirer');
+
+const startQuestions = [
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project\'s title? (Required)',
+        validate: title => {
+            if (title) {
+                return true;
+            } else {
+                console.log('Please enter a project title!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What does your project do? Enter a short description:'
+    },
+    {
+        type: 'confirm',
+        name: 'installation_stringOrList',
+        message: `~~ Installation ~~
+? >>> Are multiple steps required to install your application?`
+    }
+];
+
+const stringQuestions = [
+    {
+        type: 'confirm',
+        name: 'confirmCommand',
+        message: '>>> Is this a command to be run in terminal?'
+    },
+    {
+        type: 'input',
+        name: 'listItem',
+        message: '>>> Please type the command as it would be entered:',
+        when: answers => answers.confirmCommand
+    },
+    {
+        type: 'input',
+        name: 'listItem',
+        message: '>>> Please enter the instructions:',
+        when: answers => !answers.confirmCommand
+    }
+];
+
+const listQuestions = [
+    {
+        type: 'confirm',
+        name: 'confirmCommand',
+        message: '>>> Is this step a command to be run in terminal?'
+    },
+    {
+        type: 'input',
+        name: 'listItem',
+        message: '>>> Enter command:',
+        when: answers => answers.confirmCommand
+    },
+    {
+        type: 'input',
+        name: 'listItem',
+        message: '>>> Enter instruction:',
+        when: answers => !answers.confirmCommand
+    },
+    {
+        type: 'confirm',
+        name: 'confirmMoreSteps',
+        message: '>>> Would you like to add another step?',
+        default: false
+    }
+];
+
+const endQuestions = [    
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license does your project have?',
+        choices: ['MIT','APACHE 2.0','GPL 3.0','BSD 3','NONE']
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'What does the user need to know about contributing to the repo?'
+    },
+    {
+        type: 'confirm',
+        name: 'test_stringOrList',
+        message: `~~ Tests ~~
+? >>> Are multiple steps required to test your application?`
+    }
+];
+
+const startPrompt = () => inquirer.prompt(startQuestions);
+
+const promptList = (data, arrName) => {
+    if (!data[arrName]) {data[arrName] = []};
+    return inquirer.prompt(listQuestions)
+        .then(answers => {
+            data[arrName].push(answers);
+            if (answers.confirmMoreSteps) {
+                return promptList(data, arrName)
+            } else {
+                return data
+            };
+        });
+};
+
+const promptString = (data, arrName) => {
+    return inquirer.prompt(stringQuestions)
+        .then(answers => {
+            data[arrName] = answers;
+            return data
+        });
+}
+
+const usagePrompt = data => {
+    return inquirer.prompt([
+        {
+            type: 'confirm',
+            name: 'usage_stringOrList',
+            message: `~~ Usage ~~
+? >>> Are multiple steps required to use your application?`
+        }
+    ])
+        .then(answers => {
+            return Object.assign(data,answers)
+        });
+};
+
+const finalPrompts = data => {
+    return inquirer.prompt(endQuestions)
+        .then(answers => {
+            return Object.assign(data,answers)
+        });
+};
+
+module.exports = () => {
+    return startPrompt()
+        .then(data => {
+            if (data.installation_stringOrList) {
+                return promptList(data, 'installation');
+            } else {return promptString(data, 'installation')};
+        })
+        .then(usagePrompt)
+        .then(data => {
+            if (data.usage_stringOrList) {
+                return promptList(data, 'usage');
+            } else {return promptString(data, 'usage')};
+        })
+        .then(finalPrompts)
+        .then(data => {
+            if (data.test_stringOrList) {
+                return promptList(data, 'test');
+            } else {return promptString(data, 'test')};
+        })
+};
